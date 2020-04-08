23a3a128e32297dc6bc1598d8cc296ce6f1b0ee6
joao nogueira <joaomenna1@gmail.com>
2020-04-08 03:58:41 -0400

    create components board, card, list and header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import  HTML5Backend  from "react-dnd-html5-backend";
 
 import GlobalStyle from './styles/globalStyles';
 import Header from './components/Header';
+import Board from './components/Board';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
+    <DndProvider backend={HTML5Backend}>
       <Header />
+      <Board />
+      
+      <GlobalStyle />
 
-    </>
+    </DndProvider>
   );
 }
 
